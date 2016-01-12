a39858e324058c1dc920e81dd0a75fc439ca62dc
James Shih <gtx1024@gmail.com>
2016-01-12 11:13:55 +0800

    Variable rename

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     return 400 if song.nil?
     return 400 unless song[:type].start_with? 'audio/'
     filename = Digest::MD5.hexdigest "#{request.ip}|#{Time.now.to_i}"
-    originName = File.basename song[:filename], '.*'
+    origin_name = File.basename song[:filename], '.*'
     temp_file_path = "./temp/#{filename}"
 
     File.open(temp_file_path, 'wb') do |f|
@@ -33,13 +33,13 @@ class App < Sinatra::Base
 
     {
       filename: filename,
-      originName: originName,
+      originName: origin_name,
       duration: song.duration
     }.to_json
   end
 
   post '/render' do
-    filename, originName = params[:filename], params[:originName]
+    filename, origin_name = params[:filename], params[:originName]
     temp_file_path = "./temp/#{filename}"
     ringtone_path = "#{temp_file_path}.m4a"
     ss, to = params[:range].split(',').map(&:to_i)
@@ -49,6 +49,6 @@ class App < Sinatra::Base
 
     song.transcode ringtone_path, "-c:a libfdk_aac -ss #{ss} -to #{to} -vn -sn -y"
 
-    send_file ringtone_path, filename: "#{originName}.m4r", type: 'audio/MP4A-LATM'
+    send_file ringtone_path, filename: "#{origin_name}.m4r", type: 'audio/MP4A-LATM'
   end
 end
